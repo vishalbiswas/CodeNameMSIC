25c56bb2fd462be2aad27e99f7d7e3ba83095c4d
Vishal Biswas <vshlbsiwas@ymail.com>
2015-04-02 21:46:41 +0530

    icons,more controls,bug fixes

--- CodeNameMSIC/Form1.Designer.cs
+++ CodeNameMSIC/Form1.Designer.cs
@@ -37,13 +37,15 @@
 			this.album = new System.Windows.Forms.PictureBox();
 			this.playlist = new System.Windows.Forms.ListView();
 			this.Title = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.nbut = new System.Windows.Forms.Button();
+			this.pbut = new System.Windows.Forms.Button();
 			((System.ComponentModel.ISupportInitialize)(this.seek)).BeginInit();
 			((System.ComponentModel.ISupportInitialize)(this.album)).BeginInit();
 			this.SuspendLayout();
 			// 
 			// seek
 			// 
-			this.seek.Location = new System.Drawing.Point(104, 154);
+			this.seek.Location = new System.Drawing.Point(176, 240);
 			this.seek.Maximum = 100;
 			this.seek.Name = "seek";
 			this.seek.Orientation = System.Windows.Forms.Orientation.Vertical;
@@ -55,32 +57,32 @@
 			// ppbut
 			// 
 			this.ppbut.Enabled = false;
-			this.ppbut.Location = new System.Drawing.Point(23, 183);
+			this.ppbut.Image = global::CodeNameMSIC.Properties.Resources._1428006213_208018;
+			this.ppbut.Location = new System.Drawing.Point(67, 225);
 			this.ppbut.Name = "ppbut";
-			this.ppbut.Size = new System.Drawing.Size(75, 23);
+			this.ppbut.Size = new System.Drawing.Size(40, 40);
 			this.ppbut.TabIndex = 2;
-			this.ppbut.Text = "Play";
 			this.ppbut.UseVisualStyleBackColor = true;
 			this.ppbut.Click += new System.EventHandler(this.play);
 			// 
 			// button3
 			// 
 			this.button3.Enabled = false;
-			this.button3.Location = new System.Drawing.Point(23, 212);
+			this.button3.Image = global::CodeNameMSIC.Properties.Resources._1428006226_208014;
+			this.button3.Location = new System.Drawing.Point(67, 271);
 			this.button3.Name = "button3";
-			this.button3.Size = new System.Drawing.Size(75, 23);
+			this.button3.Size = new System.Drawing.Size(40, 40);
 			this.button3.TabIndex = 3;
-			this.button3.Text = "Stop";
 			this.button3.UseVisualStyleBackColor = true;
 			this.button3.Click += new System.EventHandler(this.stop);
 			// 
 			// button1
 			// 
-			this.button1.Location = new System.Drawing.Point(23, 154);
+			this.button1.Image = global::CodeNameMSIC.Properties.Resources._1428006297_208033;
+			this.button1.Location = new System.Drawing.Point(67, 179);
 			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.Size = new System.Drawing.Size(40, 40);
 			this.button1.TabIndex = 4;
-			this.button1.Text = "Open";
 			this.button1.UseVisualStyleBackColor = true;
 			this.button1.Click += new System.EventHandler(this.open);
 			// 
@@ -92,7 +94,7 @@
 			// 
 			// volumeT
 			// 
-			this.volumeT.Location = new System.Drawing.Point(130, 183);
+			this.volumeT.Location = new System.Drawing.Point(257, 273);
 			this.volumeT.Name = "volumeT";
 			this.volumeT.Size = new System.Drawing.Size(36, 23);
 			this.volumeT.TabIndex = 5;
@@ -113,10 +115,10 @@
 			this.playlist.AutoArrange = false;
 			this.playlist.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.Title});
-			this.playlist.Location = new System.Drawing.Point(174, 13);
+			this.playlist.Location = new System.Drawing.Point(193, 13);
 			this.playlist.MultiSelect = false;
 			this.playlist.Name = "playlist";
-			this.playlist.Size = new System.Drawing.Size(219, 222);
+			this.playlist.Size = new System.Drawing.Size(200, 222);
 			this.playlist.TabIndex = 8;
 			this.playlist.UseCompatibleStateImageBehavior = false;
 			this.playlist.View = System.Windows.Forms.View.List;
@@ -125,16 +127,40 @@
 			// 
 			this.Title.Width = 219;
 			// 
+			// nbut
+			// 
+			this.nbut.Enabled = false;
+			this.nbut.Image = global::CodeNameMSIC.Properties.Resources._1428006282_208015;
+			this.nbut.Location = new System.Drawing.Point(113, 225);
+			this.nbut.Name = "nbut";
+			this.nbut.Size = new System.Drawing.Size(40, 40);
+			this.nbut.TabIndex = 2;
+			this.nbut.UseVisualStyleBackColor = true;
+			this.nbut.Click += new System.EventHandler(this.next);
+			// 
+			// pbut
+			// 
+			this.pbut.Enabled = false;
+			this.pbut.Image = global::CodeNameMSIC.Properties.Resources._1428006291_208016;
+			this.pbut.Location = new System.Drawing.Point(22, 225);
+			this.pbut.Name = "pbut";
+			this.pbut.Size = new System.Drawing.Size(40, 40);
+			this.pbut.TabIndex = 2;
+			this.pbut.UseVisualStyleBackColor = true;
+			this.pbut.Click += new System.EventHandler(this.previous);
+			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(405, 246);
+			this.ClientSize = new System.Drawing.Size(405, 333);
 			this.Controls.Add(this.playlist);
 			this.Controls.Add(this.album);
 			this.Controls.Add(this.volumeT);
 			this.Controls.Add(this.button1);
 			this.Controls.Add(this.button3);
+			this.Controls.Add(this.pbut);
+			this.Controls.Add(this.nbut);
 			this.Controls.Add(this.ppbut);
 			this.Controls.Add(this.seek);
 			this.Name = "Form1";
@@ -157,6 +183,8 @@
 		private System.Windows.Forms.PictureBox album;
 		private System.Windows.Forms.ListView playlist;
 		private System.Windows.Forms.ColumnHeader Title;
+		private System.Windows.Forms.Button nbut;
+		private System.Windows.Forms.Button pbut;
 	}
 }
 
--- CodeNameMSIC/Form1.cs
+++ CodeNameMSIC/Form1.cs
@@ -17,6 +17,8 @@ namespace CodeNameMSIC
     {
         IWavePlayer waveOutDevice = new WaveOut();
         AudioFileReader audioFileReader;
+		int current = 0;
+		string[] files;
 
         public Form1()
         {
@@ -26,49 +28,87 @@ namespace CodeNameMSIC
 
         private void open(object sender, EventArgs e)
         {
-            file.ShowDialog();
-            try
-            {
-				foreach(string fileN in file.FileNames)
+			DialogResult res = file.ShowDialog();
+			if (res==DialogResult.OK)
+			{
+				files = file.FileNames;
+				try
 				{
-					playlist.Items.Add(new ListViewItem(TagLib.File.Create(fileN).Tag.Title));
+					foreach (string fileN in files)
+					{
+						playlist.Items.Add(new ListViewItem(TagLib.File.Create(fileN).Tag.Title));
 
+					}
+					audioFileReader = new AudioFileReader(files[0]);
+					TagLib.File f = TagLib.File.Create(files[0]);
+					Text = f.Tag.Title;
+					album.Image = Image.FromStream(new MemoryStream(f.Tag.Pictures[0].Data.Data)).GetThumbnailImage(album.Width, album.Height, null, IntPtr.Zero);
+					waveOutDevice.Init(audioFileReader);
+					ppbut.Enabled = button3.Enabled = pbut.Enabled = nbut.Enabled = true;
 				}
-                audioFileReader = new AudioFileReader(file.FileNames[0]);
-				TagLib.File f = TagLib.File.Create(file.FileNames[0]);
+				catch { }
+			}
+        }
+
+		private void next(object sender, EventArgs e)
+		{
+			if (current == file.FileNames.Length-1) current = 0;
+			else current++;
+			stop(new object(), new EventArgs());
+			try
+			{
+				audioFileReader = new AudioFileReader(files[current]);
+				TagLib.File f = TagLib.File.Create(files[current]);
 				Text = f.Tag.Title;
 				album.Image = Image.FromStream(new MemoryStream(f.Tag.Pictures[0].Data.Data)).GetThumbnailImage(album.Width, album.Height, null, IntPtr.Zero);
-                waveOutDevice.Init(audioFileReader);
-                ppbut.Enabled = button3.Enabled = true;
-            }
-            catch {	}
-        }
+				waveOutDevice.Init(audioFileReader);
+				play(new object(), new EventArgs());
+			}
+			catch { }
+		}
 
         private void play(object sender, EventArgs e)
         {
             if (waveOutDevice.PlaybackState == NAudio.Wave.PlaybackState.Playing)
             {
                 waveOutDevice.Pause();
-                ppbut.Text = "Play";
+				ppbut.Image = Properties.Resources._1428006213_208018;
             }
             else
             {
                 waveOutDevice.Play();
-                ppbut.Text = "Pause";
-            }
+				ppbut.Image = Properties.Resources._1428006230_208019;
+			}
         }
 
         private void stop(object sender, EventArgs e)
         {
             waveOutDevice.Stop();
             audioFileReader.Position = 0;
-            ppbut.Text = "Play";
-        }
+			ppbut.Image = Properties.Resources._1428006213_208018;
+		}
 
         private void seekTo(object sender, EventArgs e)
         {
             waveOutDevice.Volume = ((float)seek.Value)/100;
 			volumeT.Text = (waveOutDevice.Volume*100).ToString()+"%";
         }
-    }
+
+		private void previous(object sender, EventArgs e)
+		{
+			if (current == 0) current = file.FileNames.Length - 1;
+			else current--;
+			stop(new object(), new EventArgs());
+			try
+			{
+				audioFileReader = new AudioFileReader(files[current]);
+				TagLib.File f = TagLib.File.Create(files[current]);
+				Text = f.Tag.Title;
+				album.Image = Image.FromStream(new MemoryStream(f.Tag.Pictures[0].Data.Data)).GetThumbnailImage(album.Width, album.Height, null, IntPtr.Zero);
+				waveOutDevice.Init(audioFileReader);
+				play(new object(), new EventArgs());
+			}
+			catch { }
+		}
+	}
 }
